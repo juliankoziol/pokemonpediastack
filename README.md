02ded1e86edf49e32986932ecc16da55817acd6e
juliankoziol <julian.krzysztof.koziol@gmail.com>
2021-03-07 14:13:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pokemonpediajuliana-hk3tpv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pokemonpediajuliana-hk3tpv)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, MatToolbarModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
